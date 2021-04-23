9064387f24f3647399bb11c73b613c526c3c4f51
Harshit1123 <76706344+Harshit1123@users.noreply.github.com>
2021-04-23 16:27:49 +0530

    Create 231A_team.c

---
+++ Solutions/2/231A_team.c
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+ int n,s=0,p=0;;
+ cin>>n;
+ int a[3];
+ while(n--){
+ for (int i = 0; i < 3; i++) {
+    cin>>a[i];
+ }
+ 
+ for (int j = 0; j < 3; j++) {
+    if(a[j]==1)
+        s++;
+ }
+ if(s>1)
+    p++;
+    
+ s=0;    
+ };
+ cout<<p;
+return 0; 
+}
