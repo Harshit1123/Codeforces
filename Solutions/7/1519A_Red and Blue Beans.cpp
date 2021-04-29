6eed910f46c2fbbe4f2db18eb45c6ddc8e98fa02
Harshit1123 <76706344+Harshit1123@users.noreply.github.com>
2021-04-29 23:43:00 +0530

    Create 1519A_Red and Blue Beans.cpp

---
+++ Solutions/7/1519A_Red and Blue Beans.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Y cout<<"YES\n";
+#define N cout<<"NO\n";
+
+void solve(){
+ll i,n,k,m,p=0,l,j,t1=0,t2=0,r=0,t,b,c;
+cin>>l>>r>>n;;
+ 
+t1=(max(l,r)-min(l,r));
+t2=(t1+min(l,r)-1)/min(l,r);
+ 
+ 
+if(t2<=n)
+ Y
+else
+ N
+}
+ 
+ 
+int main(){
+long long a;
+cin>>a;
+while(a--)
+solve();
+ 
+ 
+return 0;
+}
+ 
+ 
