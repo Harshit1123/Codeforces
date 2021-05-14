56019da877dff10e1943b206580585435c5ba62c
Harshit1123 <76706344+Harshit1123@users.noreply.github.com>
2021-05-14 23:24:55 +0530

    Update and rename 158A_NextRound.c to 158A_NextRound.cpp

--- Solutions/3/158A_NextRound.cpp
+++ Solutions/3/158A_NextRound.cpp
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
  
 int a[101];
 int n,l,k;
  
 int main()
 { 
-scanf("%d%d",&n,&k);
+cin>>n>>k;
 for(int i=0;i<n;i++)
- scanf("%d",&a[i]);
+cin>>a[i];
  
 int scr=a[k-1];
 for(int i=0;i<n;i++)
     if(a[i]>=scr&&a[i]!=0)l++;
-    printf("%d",l);
+  cout<<l;
  }  
